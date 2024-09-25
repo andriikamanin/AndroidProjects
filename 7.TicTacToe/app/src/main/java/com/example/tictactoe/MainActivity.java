00c9959675f68a7d2e5b34015341e243b2f8994c
Andrii <148819392+andriikamanin@users.noreply.github.com>
2024-09-25 19:03:46 +0200

    добавлена логика игры

--- 7.TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ 7.TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -15,14 +15,12 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    final static int WIDTH = 3;
-    final static int HEIGHT = 3;
+    final static int SIZE = 3;
 
-    private ImageView img1, img2,img3,img4,img5, img6,img7, img8,img9;
-    Boolean player = false;
+    private ImageView[][] board = new ImageView[SIZE][SIZE]; // Матрица для игрового поля
+    private int[][] gameState = new int[SIZE][SIZE]; // Матрица для состояния игры (-1: пусто, 1: X, 0: O)
 
-    private ArrayList<String> tria = new ArrayList<String>();
-
+    Boolean player = false; // false - O, true - X
     TextView txtRisultato;
 
     String nome1 = "Roberto";
@@ -34,135 +32,111 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        img1 = findViewById(R.id.img1);
-        img2 = findViewById(R.id.img2);
-        img3 = findViewById(R.id.img3);
-        img4 = findViewById(R.id.img4);
-        img5 = findViewById(R.id.img5);
-        img6 = findViewById(R.id.img6);
-        img7 = findViewById(R.id.img7);
-        img8 = findViewById(R.id.img8);
-        img9 = findViewById(R.id.img9);
+        // Инициализация ImageView для игрового поля
+        board[0][0] = findViewById(R.id.img1);
+        board[0][1] = findViewById(R.id.img2);
+        board[0][2] = findViewById(R.id.img3);
+        board[1][0] = findViewById(R.id.img4);
+        board[1][1] = findViewById(R.id.img5);
+        board[1][2] = findViewById(R.id.img6);
+        board[2][0] = findViewById(R.id.img7);
+        board[2][1] = findViewById(R.id.img8);
+        board[2][2] = findViewById(R.id.img9);
 
-
-        for(int i = 0; i<WIDTH*HEIGHT; i++){
-            tria.add("-1");
+        // Инициализация состояния игры
+        for(int i = 0; i < SIZE; i++) {
+            for(int j = 0; j < SIZE; j++) {
+                gameState[i][j] = -1; // Изначально все клетки пусты
+            }
         }
 
-        img1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(0);
+        txtRisultato = findViewById(R.id.txtRisultato);
+
+        // Установка обработчиков нажатий
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                final int row = i;
+                final int col = j;
+                board[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        makeMove(row, col);
+                    }
+                });
             }
-        });
-        img2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(1);
-            }
-        });
-        img3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(2);
-            }
-        });
-        img4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(3);
-            }
-        });
-        img5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(4);
-            }
-        });
-        img6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(5);
-            }
-        });
-        img7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(6);
-            }
-        });
-        img8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(7);
-            }
-        });
-        img9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mossa(8);
-            }
-        });
+        }
     }
-    public void mossa(int m) {
-        if(tria.get(m).equals("-1")) {
+
+    public void makeMove(int row, int col) {
+        if(gameState[row][col] == -1) { // Если клетка пуста
             if(player) {
-                switch (m) {
-                    case 0: img1.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 1: img2.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 2: img3.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 3: img4.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 4: img5.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 5: img6.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 6: img7.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 7: img8.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                    case 8: img9.setImageDrawable(getDrawable(R.drawable.mossa_x)); break;
-                }
-                tria.set(m,"1");
+                board[row][col].setImageDrawable(getDrawable(R.drawable.mossa_x));
+                gameState[row][col] = 1; // Ход X
+            } else {
+                board[row][col].setImageDrawable(getDrawable(R.drawable.mossa_o));
+                gameState[row][col] = 0; // Ход O
             }
-            else {
-                switch (m) {
-                    case 0:
-                        img1.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 1:
-                        img2.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 2:
-                        img3.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 3:
-                        img4.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 4:
-                        img5.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 5:
-                        img6.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 6:
-                        img7.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 7:
-                        img8.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                    case 8:
-                        img9.setImageDrawable(getDrawable(R.drawable.mossa_o));
-                        break;
-                }
-                tria.set(m, "0");
+
+            if (checkWinner()) { // Проверка победителя после каждого хода
+                String winner = player ? nome1 : nome2;
+                txtRisultato.setText(winner + " победил!");
+                disableBoard(); // Отключить поле после победы
+            } else if (isBoardFull()) {
+                txtRisultato.setText("Ничья!");
+            } else {
+                player = !player; // Смена игрока
+                showCurrentPlayer();
             }
-            player = !player;
-            mostraGiocatore();
         }
     }
 
-    public void mostraGiocatore() {
+    public void showCurrentPlayer() {
         if(player) {
-            txtRisultato.setText("Gioca " + nome1);
+            txtRisultato.setText("Ходит " + nome1);
+        } else {
+            txtRisultato.setText("Ходит " + nome2);
         }
-        else {
-            txtRisultato.setText("Gioca " + nome2);
+    }
+
+    public boolean checkWinner() {
+        // Проверка строк
+        for (int i = 0; i < SIZE; i++) {
+            if (gameState[i][0] != -1 && gameState[i][0] == gameState[i][1] && gameState[i][1] == gameState[i][2]) {
+                return true;
+            }
+        }
+        // Проверка столбцов
+        for (int i = 0; i < SIZE; i++) {
+            if (gameState[0][i] != -1 && gameState[0][i] == gameState[1][i] && gameState[1][i] == gameState[2][i]) {
+                return true;
+            }
+        }
+        // Проверка диагоналей
+        if (gameState[0][0] != -1 && gameState[0][0] == gameState[1][1] && gameState[1][1] == gameState[2][2]) {
+            return true;
+        }
+        if (gameState[0][2] != -1 && gameState[0][2] == gameState[1][1] && gameState[1][1] == gameState[2][0]) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (gameState[i][j] == -1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void disableBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j].setEnabled(false);
+            }
         }
     }
 }
