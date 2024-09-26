a245153294ce4d92d1efff5c0ab47bcf0d4bf393
Andrii <148819392+andriikamanin@users.noreply.github.com>
2024-09-26 19:56:29 +0200

    связаны TextView, EditText, Button в MainActivity и activity_main

--- 8.GuessTheWord/app/src/main/java/com/example/a8guesstheword/MainActivity.java
+++ 8.GuessTheWord/app/src/main/java/com/example/a8guesstheword/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a8guesstheword;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView numeroTentativi;
+    private TextView parolaNascosta;
+    private EditText campoDiImmissione;
+    private Button buttonProva;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        numeroTentativi   =     findViewById(R.id.numeroTentativi);
+        parolaNascosta    =     findViewById(R.id.parolaNascosta);
+        campoDiImmissione =     findViewById(R.id.campoDiImmissione);
+        buttonProva       =     findViewById(R.id.buttonProva);
 
     }
 }
